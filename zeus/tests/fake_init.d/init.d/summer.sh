e40a54a945b31c6be03d1375deab4f051bf214b1
Eduardo L. García Glez <eduardo.l.g.g@gmail.com>
2017-04-20 10:00:39 +0100

    New structure of fake_init.d
    I have created a new structure for fake_init.d which is similar to the
    traditional init.

--- zeus/tests/fake_init.d/init.d/summer.sh
+++ zeus/tests/fake_init.d/init.d/summer.sh
@@ -14,13 +14,13 @@ cmd=""
 user=""
 
 name=`basename $0`
-stdout_log="./log/$name.log"
+stdout_log="../log/$name.log"
 timestamp=$(date)
 
 case "$1" in
     start)
         echo "Starting $name"
-		sleep 15
+		sleep 8
 		echo "Started $name"
 		echo "$timestamp Starting $name" >> $stdout_log
     ;;
@@ -37,8 +37,7 @@ case "$1" in
 		echo "$timestamp Status $name" >> $stdout_log
     ;;
     *)
-		echo "Usage: $0 {start|stop|restart|status}"
-		echo "$timestamp Usage $name" >> $stdout_log
+    echo "Usage: $0 {start|stop|restart|status}"
     exit 1
     ;;
 esac
