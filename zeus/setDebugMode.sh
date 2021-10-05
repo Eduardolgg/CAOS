7ed9703ebe7029707b0da9d9bf1fa6e85cd74de5
Eduardo L. García Glez <eduardo.l.g.g@gmail.com>
2021-10-05 21:34:47 +0100

    Set debug/release simplified

--- zeus/setDebugMode.sh
+++ zeus/setDebugMode.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-if [ "$#" -ne 0 ]; then
-  if [ "$1" != "debug" ]; then
-    echo "Usage $0 [debug]"
-    exit 1
-  else
-    debug=true
-  fi
-fi
-
-debug=${debug-false}
-
-if [ "$debug" != "true" ]; then
-  sed -i "/conf.set('DEBUG', true)/c conf.set('DEBUG', false)" include/meson.build
-else
-  sed -i "/conf.set('DEBUG', false)/c conf.set('DEBUG', true)" include/meson.build
-fi
-
 cd ./build
 meson configure -h > grep "Error"
 if [ "$?" -ne 0 ]; then
